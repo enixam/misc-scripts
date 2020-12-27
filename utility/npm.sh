e24b6e6d457398fda3f99c6d980c8e17c5d47278
qiushi <qiushi.yann@gmail.com>
2020-12-27 16:12:06 +0800

    add npm and pip

---
+++ utility/npm.sh
@@ -0,0 +1,9 @@
+
+# create package.json
+npm init
+# install a package and save it in package.json
+npm install <package-name> --save-dev
+# install dependencies (when package.json is present)
+npm install 
+# remove a dependency and save it in package.json
+npm uninstall <package-name> --save
--- utility/pip-configuration.sh
+++ utility/pip-configuration.sh
@@ -1,5 +1,5 @@
 # open pip configuration file
-notepad C:\Users\Lenovo\pip\pip.ini
+code C:\Users\Lenovo\pip\pip.ini
 # use tsinghua mirror globally, add the following lines in the above file
 [global]
 index-url=https://pypi.tuna.tsinghua.edu.cn
@@ -8,6 +8,11 @@ trusted-host = https://pypi.tuna.tsinghua.edu.cn
 
 # or use pip command
 pip config set global.index-url https://pypi.tuna.tsinghua.edu.cn/simple
+# aliyun
+pip config set global.index-url https://mirrors.aliyun.com/pypi/simple/
 
 # use tsinghua mirror once
 pip install -i https://pypi.tuna.tsinghua.edu.cn/simple some-package
+
+# install a package without C extensions
+pip install some-package --install-option="--without-c-extensions"
