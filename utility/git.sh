0c58ebcfaca62504648a99c41f648187ecc7b4ce
qiushi <qiushi.yann@gmail.com>
2020-12-29 19:11:10 +0800

    Update git.sh

--- utility/git.sh
+++ utility/git.sh
@@ -1,5 +1,8 @@
-# clear git cache
+# clear git cache of a folder
 git rm --cached filename -r 
+# clear all git cache (this will delete all folders and files)
+# you may want to use `git add` to add necessary files back after performing this operation
+git rm -r --cached
 
 # reduce git file size
 git gc --aggressive --prune=now
