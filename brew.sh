e9c9d68e2a987a7731379dbb43f930a9473deed4
qiushi <qiushi.yann@gmail.com>
2021-01-08 17:33:47 +0800

    update

---
+++ brew.sh
@@ -0,0 +1,48 @@
+# install brew
+# see mirror configuration at 
+/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+
+# uninstall homebrew (uninstall cli but not casks)
+/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/uninstall.sh)"
+
+# check install
+brew doctor
+
+# search a package (dispaly available formulae and casks)
+brew search <package>
+
+# install a package (to the usr/local/Cellar)
+brew install <package>
+
+# dispaly if a package is installed, version, location, dependencies and some analytics data
+brew info <package>
+
+# list installed packages
+brew list
+
+# uninstall a package
+brew uninstall <package>
+
+# fetch newest version of all installed packages
+brew update
+
+# actually update a package
+brew upgrade <package>
+
+# display all installed package with a newer version
+brew outdated
+
+# brew does not delete older versions of packages, brew cleanup delete the older version
+brew cleanup
+
+# install an application rather than cli (to Applications folder)
+brew cask install<app>
+
+# go to application homepage
+brew cask home <app>
+
+# install package from other repo (other than homebrew core repo)
+brew search heroku # no formula or cask found
+brew tap heroku/brew # find on heroko website
+brew search heroku # now the formula for heroku is visible 
+brew install heroku # install heroku 
--- utility/git.sh
+++ utility/git.sh
@@ -6,3 +6,9 @@ git rm -r --cached
 
 # reduce git file size
 git gc --aggressive --prune=now
+
+# clone a repo using mirror into a specific folder 
+git clone https://github.com.cnpmjs.org/repo.git <folder>
+
+# Create a shallow clone with a history truncated to the last n commits.
+git clone --depth n repo 
