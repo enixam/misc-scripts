516a96061f5bf6e8e2c3680fc9dfc8c1912062a6
qiushi <qiushi.yann@gmail.com>
2021-01-12 18:44:42 +0800

    update

---
+++ utility/firebase.sh
@@ -0,0 +1,6 @@
+# when logging in behind a proxy
+firebase login --no-localhost
+
+# for authentication problems, see https://github.com/firebase/firebase-tools/issues/155
+# current proxy setting:
+self.proxy = "http://127.0.0.1:3213"
--- utility/npm.sh
+++ utility/npm.sh
@@ -6,10 +6,11 @@ npm init
 npm install -g <package>
 
 # install a package locally and save it in package.json
-npm install --save <package>
+npm install (--save) <package>
+npm uninstall (--save) <package> 
 
 
 # install dependencies (when package.json is present)
 npm install 
 # remove a dependency and save it in package.json
-npm uninstall <package-name> --save
+
