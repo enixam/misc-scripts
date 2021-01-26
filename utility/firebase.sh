3f483bf538cbdd86bf078479575f69c79a3c5338
qiushi <qiushi.yann@gmail.com>
2021-01-26 13:54:42 +0800

    Update firebase.sh

--- utility/firebase.sh
+++ utility/firebase.sh
@@ -1,6 +1,7 @@
-# the core package
+# install the core package
 npm i -g firebase
 
+# use other services
 # to prevent login issues for hosting
 npm i -g firebase-tools@8.15.0
 
@@ -10,3 +11,6 @@ firebase login --no-localhost
 # for authentication problems, see https://github.com/firebase/firebase-tools/issues/155
 # current proxy setting:
 self.proxy = "http://127.0.0.1:3213"
+
+# generate token 
+firebase login:ci
